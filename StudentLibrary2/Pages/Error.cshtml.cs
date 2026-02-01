9c98ae4d7e074fef68f2133eb1027e2ee6450aca
Артур Стариков <Gibyte.mail@gmail.com>
2026-02-01 20:55:32 +0300

    4

--- StudentLibrary2/Pages/Error.cshtml.cs
+++ StudentLibrary2/Pages/Error.cshtml.cs
@@ -8,8 +8,6 @@ namespace StudentLibrary2.Pages;
 [IgnoreAntiforgeryToken]
 public class ErrorModel : PageModel
 {
-
-
     public string? RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
